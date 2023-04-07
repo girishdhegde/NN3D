86de1f8d8a82049907a56ac89fa76977bb61de5e
Girish Hegde <girish.dhc@gmail.com>
2023-04-07 12:56:45 +0530

    create file

---
+++ nerf/graphics.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+import json
+
+import numpy as np
+import cv2
+import einops
+import torch
+
+
+__author__ = "__Girish_Hegde__"
+
+
+class VolumeRenderer:
+    def __init__(self):
+        pass
+    
+    def generate_rays(self, ): pass
+
+    def render(self): pass
