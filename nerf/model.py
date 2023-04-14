e853da846ae1029b6cb61697550e56d23c06e8c6
Girish Hegde <girish.dhc@gmail.com>
2023-04-14 18:51:31 +0530

    model.py initial commit

---
+++ nerf/model.py
@@ -0,0 +1,14 @@
+import numpy as np
+from einops import repeat, rearrange
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+__author__ = "__Girish_Hegde__"
+
+
+class MLP(nn.Module): pass
+
+
+class NeRF: pass
